bb850ff207e272e523cea7bed5be3210a1accb9f
Rachmat Priambudi <rachmat.keybladers93@gmail.com>
2019-11-22 11:16:50 +0700

    - fix validate macro - change from returning value to directly set value to request - return error response if not authorized

--- providers/RamenProvider.js
+++ providers/RamenProvider.js
@@ -2,30 +2,35 @@ const { ServiceProvider } = require('@adonisjs/fold')
 const axios = require('axios')
 
 class RamenProvider extends ServiceProvider {
-  boot() {
-    const Request = use('Adonis/Src/Request')
-    const Config = use('Adonis/Src/Config')
-
-    Request.macro('validate', async function () {
-        const authUrl = Config._config.ramen.authUrl
-        let claim = this.url()
-        let token = this.header('Authorization')
-        token = token.split(' ')
-        token = token[1]
-        const body = {
-            claim: claim,
-            token: token
-        }
-
-        try {
-            const { data } = await axios.post(authUrl, body)
-            return data.data
-        }
-        catch(error) {
-            return
-        }
-    })
-  }
+    boot() {
+        const Request = use('Adonis/Src/Request')
+        const Config = use('Adonis/Src/Config')
+    
+        Request.macro('validate', async function (response) {
+            const authUrl = Config._config.ramen.authUrl
+            let claim = this.url()
+            let token = this.header('Authorization')
+            token = token.split(' ')
+            token = token[1]
+            const body = {
+                claim: claim,
+                token: token
+            }
+    
+            try {
+                const { data } = await axios.post(authUrl, body)
+                this.loginData = data.data
+            }
+            catch(error) {
+                return response.status(403).send({
+                    data: null,
+                    meta: {
+                        message: 'You\'re not authorized'
+                    }
+                })
+            }
+        })
+    }
 
   register() {
       this.app.singleton('RamenQueryResolver', (app) => {
