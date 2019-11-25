b47fc4f25b7f5e20ea3cb3f3ef8899ab25c013f9
Rachmat Priambudi <rachmat.keybladers93@gmail.com>
2019-11-25 10:54:47 +0700

    add trait path to config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
 module.exports = {
-    authUrl: 'http://localhost:3333/api/auth/verify'
+    authUrl: 'http://localhost:3333/api/auth/verify',
+    traitPath: '../../../node_modules/ramen-adonis/src/traits/RamenModel'
 }
